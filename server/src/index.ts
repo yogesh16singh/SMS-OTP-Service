9d43330a5b044ef1db5f3fe0b6f66f4619fae34b
Yogesh Singh <yogesh.singh@corider.in>
2024-09-22 11:31:36 +0530

    frontend url change

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ const app=express();
 app.use(express.json());
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://otp-sms-system.vercel.app'], 
+    origin: ['http://localhost:5173', 'https://sms-otp-service.vercel.app/'], 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true 
